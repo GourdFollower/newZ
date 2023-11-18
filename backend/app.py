e7ad79d0298bf8a57e9b735831a771cdbd3d6319
Tessa Davis <tkdavis@eastlink.ca>
2023-11-18 16:23:18 -0500

    example endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
+def update_database(name):
+    conn = psycopg2.connect("dbname=newz user=postgres")
+    cur = conn.cursor()
 
-@app.route('/')
+    cur.execute("DROP TABLE IF EXISTS test;")
+    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, data varchar);")
+
+    cur.execute("INSERT INTO test (data) VALUES (%s)", (name,))
+    conn.commit()
+    
+    cur.close()
+    conn.close()
+
+
+
+@app.route('/', methods=['POST'])
 def hello():
+    name = request.json['name']
+    update_database(name)
     return 'Hello, World!'
