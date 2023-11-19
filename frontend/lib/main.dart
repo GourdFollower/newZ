4f6d0559a620b1ae1e4c378a3b39734453e3eefc
Rylan Donohoe <rylan.donohoe@outlook.com>
2023-11-18 22:20:36 -0500

    update

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -9,6 +9,7 @@ class NavigationBarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: const NavigationExample(),
     );
